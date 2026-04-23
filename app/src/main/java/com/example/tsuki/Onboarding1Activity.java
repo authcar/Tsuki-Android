bac0bfc86743632b0e2519c417f35df1aa4cdfec
authcar@gmail.com <youremail@example.com>
2026-04-23 18:05:19 +0700

    first commit

---
+++ app/src/main/java/com/example/tsuki/Onboarding1Activity.java
@@ -0,0 +1,52 @@
+package com.example.tsuki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Onboarding1Activity extends AppCompatActivity {
+
+    Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_onboarding1);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+                    return insets;
+                }
+        );
+
+        // INI DITAMBAH DI SINI
+        next = findViewById(R.id.btnNext);
+
+        next.setOnClickListener(v -> {
+            Intent i = new Intent(
+                    Onboarding1Activity.this,
+                    Onboarding2Activity.class
+            );
+            startActivity(i);
+        });
+
+    }
+}
